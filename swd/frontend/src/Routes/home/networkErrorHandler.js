c07fc36aea93eca3418f1ffd4fbe3b40a7f499da
Prudhvi Rampey <prudhvi.rampey@gmail.com>
2018-02-15 12:14:01 +0530

    Refactor NetworkHandler and add functioning checks for loading and error state

--- swd/frontend/src/Routes/home/networkErrorHandler.js
+++ swd/frontend/src/Routes/home/networkErrorHandler.js
@@ -1,49 +1,65 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-function networkErrorHandler (FallbackComponent, Component) {
+function networkErrorHandler(LoadingComponent, FallbackComponent, Component) {
   class NetworkErrorHandler extends React.Component {
-    constructor () {
+    constructor() {
       super()
       // Construct the initial state
       this.state = {
+        isLoading: true,
         hasError: false
       }
     }
 
-    componentWillMount (error, info) {
-
-      for (var data in this.props)
-      {
-          if(data.error || !data)
-          {
-            // Update state if error happens
-            this.setState({ hasError: true })
-          }
-          
-      }  
-      
-
-    //   // Report errors
-    //   errorCallback(error, info, this.props)
-    }
-
-    render () {
-      // if state contains error we render fallback component
-      if (this.state.hasError) {
-        return (
-          <FallbackComponent />
-        )
+    // Flow of props passed by GrapQL HoC: loading: true, no data loading : false,
+    // data is available and good
+    componentWillReceiveProps(nextProps) {
+      console.log("HoC view of props")
+      console.log(nextProps)
+      for (var key in nextProps) {
+        let query = nextProps[key]
+        console.log(query)
+        // Need to also consider the case of '!query'
+        if (query.error) {
+          // Update state if error happens
+          this.setState({hasError: true})
+          console.log("Error while loading data!")
+          console.log(query.error)
+        } else if (query.loading) {
+          this.setState({isLoading: true})
+          console.log("Data is loading")
+        } else {
+          this.setState({isLoading: false})
+          console.log("Data seems to have loaded")
+        }
       }
 
-      return <Component {...this.props} />
+      //     if(data.error || !data)     {       // Update state if error happens
+      //  this.setState({ hasError: true })     } }
+
+    }
+
+    // Report errors if any
+// Here's a good link to follow:
+// https://codeburst.io/catching-exceptions-using-higher-order-components-in-reac
+// t-16-b8a401853a10
+    //errorCallback(error, info, this.props)
+
+    render() {
+      // if state contains error we render fallback component
+      if (this.state.hasError) {
+        // Errors can be further split into GraphQL and Network errors
+        return (<FallbackComponent/>)
+      } else if (this.state.isLoading) {
+        return (<LoadingComponent/>)
+      } else {
+        return (<Component {...this.props}/>)
+      }
     }
   }
   NetworkErrorHandler.displayName = `networkErrorHandler(${Component.displayName})`
   return NetworkErrorHandler
 }
 
-const fallback = props => (
-    <h1>Something Went Wrong</h1>
-)
-
-export default networkErrorHandler(fallback);
+export default networkErrorHandler;
